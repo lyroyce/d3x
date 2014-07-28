a5bf5e8885ed5c537b04883492108a63f8230d85
Yin Li <lyroyce@sina.com>
2014-07-28 17:12:45 +0800

    Update demo.js

--- example/demo.js
+++ example/demo.js
@@ -19,7 +19,9 @@ var hbar = new d3x(".wrapper > section")
 var line = new d3x(".wrapper > section")
     .size("100%",400)
     .marginTop(40).marginLeft(40)
-    .data(["61","9","43","31","17"])
+    .data([{"name":"Jan", "value": "61"},{"name":"Feb", "value": "75"},{"name":"Mar", "value": "99"},{"name":"Apr", "value": "93"},{"name":"May", "value": "125"}])
+    .name(function(d,i){return d.name;})
+    .value(function(d,i){return +d.value;})
     .nameAxis(true, "Name")
     .valueAxis(true, "Value")
     .title("Line")
@@ -30,18 +32,18 @@ var dataFunc = function(){
         dataset = [];
     for (var i = 0; i < numValues; i++) {
         var newNumber = Math.floor(Math.random() * 25);
-        dataset.push({name:i,count:newNumber});
+        dataset.push({index:i, count:newNumber});
     }
     return dataset;
 }
 var chart = new d3x().appendTo(".wrapper > section")
     .size("100%",400)
     .data(dataFunc)
-    .name(function(d,i){return d.name;})
+    .name(function(d,i){return d.index;})
     .value(function(d,i){return d.count;})
     .margin(40, 20, 50, 40)
-    .nameAxis(true, "Name").valueAxis(true, "Count")
+    .nameAxis(true, "Index").valueAxis(true, "Count")
     .title("Auto Refreshed Chart")
-    .colors("darkorange", "darkorange", "darkorange", "darkorange")
+    .colors("darkorange", "lightgray", "dimgray", "#333")
     .refresh(5000)
     .hbar();
