ee95e4a3c6af87081890c1868d56471bb179750e
Yin Li <lyroyce@sina.com>
2014-07-28 16:59:57 +0800

    Update demo.js

--- example/demo.js
+++ example/demo.js
@@ -1,9 +1,29 @@
 //vbar
-var vbar = new d3x(".wrapper section").size("100%",400).data(["61","9","43","31","17"]).marginTop(40).marginLeft(40).nameAxis().title("Vertical Bar").vbar();
+var vbar = new d3x("#customerTrend")
+    .size("100%",400)
+    .data(["61","9","43","31","17"])
+    .marginTop(40).marginLeft(40)
+    .title("Vertical Bar")
+    .vbar();
 //hbar
-var hbar = new d3x(".wrapper section").size("100%",400).data(["61","9","43","31","17"]).marginTop(40).marginLeft(40).nameAxis().valueAxis().title("Horizontal Bar").hbar();
+var hbar = new d3x("#customerTrend")
+    .size("100%",400)
+    .marginTop(40).marginLeft(40)
+    .data([{"name":"2012", "value": 61},{"name":"2013", "value": 75},{"name":"2014", "value": 99}])
+    .name(function(d,i){return d.name;})
+    .value(function(d,i){return d.value;})
+    .nameAxis().valueAxis()
+    .title("Horizontal Bar")
+    .hbar();
 //line
-var line = new d3x(".wrapper section").size("100%",400).data(["61","9","43","31","17"]).marginTop(40).marginLeft(40).nameAxis(true).valueAxis(true).title("Line").line();
+var line = new d3x("#customerTrend")
+    .size("100%",400)
+    .marginTop(40).marginLeft(40)
+    .data(["61","9","43","31","17"])
+    .nameAxis(true, "Name")
+    .valueAxis(true, "Value")
+    .title("Line")
+    .line();
 //custom
 var dataFunc = function(){
     var numValues = Math.random() * 5,
@@ -14,16 +34,14 @@ var dataFunc = function(){
     }
     return dataset;
 }
-var chart = new d3x().appendTo(".wrapper section")
+var chart = new d3x().appendTo("#customerTrend")
     .size("100%",400)
     .data(dataFunc)
     .name(function(d,i){return d.name;})
     .value(function(d,i){return d.count;})
     .margin(40, 20, 50, 40)
-    .nameAxis(true, 'Name').valueAxis(true, 'Count')
+    .nameAxis(true, "Name").valueAxis(true, "Count")
     .title("Auto Refreshed Chart")
-    .colors(["darkorange",'steelblue'])
+    .colors("darkorange", "darkorange", "darkorange", "darkorange")
     .refresh(5000)
     .hbar();
-    
-    
